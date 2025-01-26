42a6e5512c110acc3877d6b63699b84ee89c664b
AlexandraStulianec <127435535+AlexandraStulianec@users.noreply.github.com>
2025-01-27 01:43:39 +0200

    Delete project_ibt directory

--- project_ibt/frontend/src/contracts/sui.ts
+++
@@ -1,3 +0,0 @@
-export const SUI_PACKAGE_ID = "0xb02473a13464fbba68e92c4159eba604c68a45dfe2db7570bedafc0eac207f8a";
-export const SUI_MODULE_NAME = "ibt";
-export const SUI_TREASURY_CAP_ID = "0x25bfd0d6b640d63494c70a944e698009a8a583b0a8d78e1bdd3b606d92a5a8e2";
