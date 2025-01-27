3b5dfe7d376be928b79a216c212219eec849ec5c
AlexandraStulianec <127435535+AlexandraStulianec@users.noreply.github.com>
2025-01-27 01:35:29 +0100

    Delete project_ibt2 directory

--- project_ibt2/frontend/src/contracts/ethereum.ts
+++
@@ -1,9 +0,0 @@
-export const ETH_CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-
-export const ETH_CONTRACT_ABI = [
-    "function mint(address to, uint256 amount) public",
-    "function burn(address from, uint256 amount) public",
-    "function balanceOf(address account) view returns (uint256)",
-    "event TokensMinted(address indexed to, uint256 amount)",
-    "event TokensBurned(address indexed from, uint256 amount)"
-];
